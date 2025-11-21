88f312ef9556be428439629090424c1b2988783b
Daniel Enoc Rodriguez Torres <danxtive@gmail.com>
2025-11-21 18:35:07 -0500

    esta es la version desde mi pc

--- codigoBasicoC.c
+++ codigoBasicoC.c
@@ -7,5 +7,8 @@ int main(void) {
     printf("Hola, mundo!\n");
     printf("este codigo es a;adidural \n");
     printf("asdfasdfasda");
+    
+    printf("Nueva linea añadida para prueba\n");
+
     return 0;
 }
